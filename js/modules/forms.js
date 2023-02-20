f0013afdbfcecd42744479ee08f7f2e51bf2d0f8
Ihor <djigorsan@gmail.com>
2023-02-20 15:03:50 +0200

    refactoring js modules and fixed baggs

---
+++ js/modules/forms.js
@@ -0,0 +1,83 @@
+import { closeModal, openModal } from "./modal";
+
+function forms(modalTimerId) {
+	// +++++++++++++++++++++ Forms +++++++++++++++++++++++
+
+	const forms = document.querySelectorAll('form');
+
+	const message = {
+		loading: 'img/form/spinner.svg',
+		success: `Дякуємо! Ми з вами зв'яжемося`,
+		failure: 'Щось пішло не так...'
+	};
+
+	forms.forEach(item => {
+		bindPostData(item);
+	});
+
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: "POST",
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: data
+		});
+
+		return await res.json();
+	};
+
+	function bindPostData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const statusMessage = document.createElement('img');
+			statusMessage.src = message.loading;
+			statusMessage.classList.add('load');
+
+			form.insertAdjacentElement('afterend', statusMessage);
+
+			const formData = new FormData(form);
+
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+			postData('http://localhost:3000/requests', json)
+				.then(data => {
+					console.log(data);
+					showThanksModal(message.success);
+					statusMessage.remove();
+				}).catch(() => {
+					showThanksModal(message.failure);
+				}).finally(() => {
+					form.reset();
+				});
+
+		});
+	}
+
+	function showThanksModal(message) {
+		const prevModalDialog = document.querySelector('.modal__dialog');
+
+		prevModalDialog.classList.add('hide');
+		openModal('.modal', modalTimerId);
+
+		const thanksModal = document.createElement('div');
+		thanksModal.classList.add('.modal__dialog');
+		thanksModal.innerHTML = `
+				<div class="modal__content">
+					<div class="modal__close" data-close>&times;</div>
+					<div class="modal__title">${message}</div>
+				</div>
+			`;
+
+		document.querySelector('.modal').append(thanksModal);
+		setTimeout(() => {
+			thanksModal.remove();
+			prevModalDialog.classList.add('show');
+			prevModalDialog.classList.remove('hide');
+			closeModal('.modal');
+		}, 4000);
+	}
+}
+
+export default forms;
